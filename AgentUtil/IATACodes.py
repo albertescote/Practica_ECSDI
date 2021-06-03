d6795110150d54e23f0179699e92c3af3828e886
Marc Revelles Segalés <marcrevelles@hotmail.com>
2021-06-03 18:42:30 +0200

    Create "IATACodes.py"
    The file contains a python dictionary that associates a set of city names with their IATA codes.

---
+++ AgentUtil/IATACodes.py
@@ -0,0 +1,10 @@
+# Diccionario que relaciona el nombre de un conjunto de ciudades con su código IATA
+IATA = {
+    'Barcelona': 'BCN',
+    'Paris': 'PAR',
+    'Amsterdam': 'AMS',
+    'Berlin': 'BER',
+    'Dubai': 'DXB',
+    'London': 'LHR',
+    'Rome': 'FCO'
+}
